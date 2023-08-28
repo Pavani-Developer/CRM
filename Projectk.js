ee987d5f28810a8019824350d9e35abe485f28aa
vishnuvcube <143273170+vishnuvcube@users.noreply.github.com>
2023-08-28 09:27:42 +0530

    Add files via upload

--- Projectk.js
+++ Projectk.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 import './projectk.css';
 
 function Projectk() {
   return (
-    <div className="App">
-      <div className="sidebar">
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Courses</a></li>
-          <li><a href="#">Demo</a></li>
-          <li><a href="#">Interview Questions</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
+    <div className="page">
+      <div className="top-section">
+        <h1 className="company-name">V Cube Software Solutions</h1>
+      <button>Enroll now</button>
+      <button>login</button>
+
       </div>
-      <div className='right'>
-        <div className='box'>
-          <label htmlFor="firstName">First Name:</label>
+      <div className="center-boxes">
+        <div className="center-box1">
+          <h1 className="blinking-text">Explore Knowledge, Ignite Innovation,<br />and Shape Tomorrow with VCubes Institution's Transformative Courses.</h1>
+        </div>
+        <div className="center-box2">
+          <h1>Enroll here</h1>
+        <label htmlFor="firstName">First Name:</label>
           <input type="text" id="firstName" placeholder="Enter your first name" name="First name" />
 
           <label htmlFor="lastName">Last Name:</label>
@@ -34,18 +34,16 @@ function Projectk() {
           <button onClick={() => {}}>Enroll</button>
         </div>
       </div>
-      <div className='content-container'>
-        <div className='fun'>
-          <h1>Explore Knowledge, Ignite Innovation,<br />and Shape Tomorrow with VCubes Institution's Transformative Courses.</h1>
-        </div>
-        <div className='button-container'>
-          <div className='button1'>
-            <button onClick={() => {}}>Enroll Now</button>
-          </div>
-          <div className='button2'>
-            <button onClick={() => {}}>Login</button>
-          </div>
-        </div>
+      <div className="left-section">
+
+      <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Courses</a></li>
+          <li><a href="#">Demo</a></li>
+          <li><a href="#">Interview Questions</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
       </div>
     </div>
   );
